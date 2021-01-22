2ec9daf4fa9e2ad543338112c238a95d1b3533e5
gustavobborges <gustavoborges98@hotmail.com>
2021-01-21 23:28:19 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+// import { Request, Response } from 'express';
+
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
+})
+
+app.listen(3333, () => {
+  console.log('⏩ Server started on 3333!');
+});
+
